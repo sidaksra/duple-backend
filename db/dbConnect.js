f9bdc6186deb39bf7d2eac66c24a84f96a127a24
sidaksinghsra <sidaksra8@gmail.com>
2023-11-23 20:23:13 -0500

    local db

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,15 +1,12 @@
-const { MongoClient } = require('mongodb');
-
-
-const client = new MongoClient('mongodb+srv://sidaksra8:Sidak123@cluster0.ob8gkcm.mongodb.net/?retryWrites=true&w=majority');
-
-const connectDB = async () => {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB Atlas");
-    } catch (error) {
-        console.error("Error connecting to MongoDB Atlas: ", error);
+const mongoose = require('mongoose');
+const connectDB = async() =>{
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/duple');
+        console.log("Connected to MongoDB");
     }
+    catch(error){
+        console.log("Error connecting to MongoDB: ", error);
+    }  
 }
 
 module.exports = connectDB;
